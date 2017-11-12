ce04ab15bd2748c4777170b22d4d05dc0fd36f5d
Bruce Li <405371160@qq.com>
2017-11-12 13:19:17 +0800

    创投圈投资人VO

--- src/main/java/com/thzj/webcrawler/crawler/ctq/model/investorVO.java
+++ src/main/java/com/thzj/webcrawler/crawler/ctq/model/investorVO.java
@@ -9,21 +9,21 @@ import java.util.List;
  */
 @Data
 public class investorVO {
-    private String name;                     //姓名
-    private String avatarUrl;                //头像
-    private String company;                  //所属机构
-    private String position;                 //职位
-    private String province;                 //地区
+    private String name;                                               //姓名
+    private String avatarUrl;                                          //头像
+    private String company;                                            //所属机构
+    private String position;                                           //职位
+    private String province;                                           //地区
 
-    private String detailContentUrl;         //详情URL
-    private String personalProfile;          //投资人介绍
-    private List<String> investIndustries;   //关注行业
-    private List<String> investRounds;       //投资阶段
-    private String investPlan;               //投资计划
-    private String perRoundMoney;                 //单笔可投
-    private List<InvestCase> investCase;               //投资案例
-    private List<WorkExperience> workExperiences;           //工作经历
-    private List<EducationExperience> educationExperiences;      //教育经历
+    private String detailContentUrl;                                   //详情URL
+    private String personalProfile;                                    //投资人介绍
+    private List<String> investIndustries;                             //关注行业
+    private List<String> investRounds;                                 //投资阶段
+    private String investPlan;                                         //投资计划
+    private String perRoundMoney;                                      //单笔可投
+    private List<InvestCase> investCase;                               //投资案例
+    private List<WorkExperience> workExperiences;                      //工作经历
+    private List<EducationExperience> educationExperiences;            //教育经历
 
     @Data
     public static class InvestCase {
