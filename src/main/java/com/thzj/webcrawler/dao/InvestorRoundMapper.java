901db2bc5b9133bd225f3831cba7f5875cd419b8
liqiulin <liqiulin@fxiaoke.com>
2017-11-14 21:42:54 +0800

    mybatis test code

---
+++ src/main/java/com/thzj/webcrawler/dao/InvestorRoundMapper.java
@@ -0,0 +1,33 @@
+package com.thzj.webcrawler.dao;
+
+import com.thzj.webcrawler.entity.InvestorRound;
+import com.thzj.webcrawler.entity.InvestorRoundExample;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface InvestorRoundMapper {
+    long countByExample(InvestorRoundExample example);
+
+    int deleteByExample(InvestorRoundExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(InvestorRound record);
+
+    int insertSelective(InvestorRound record);
+
+    List<InvestorRound> selectByExample(InvestorRoundExample example);
+
+    InvestorRound selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") InvestorRound record, @Param("example") InvestorRoundExample example);
+
+    int updateByExample(@Param("record") InvestorRound record, @Param("example") InvestorRoundExample example);
+
+    int updateByPrimaryKeySelective(InvestorRound record);
+
+    int updateByPrimaryKey(InvestorRound record);
+}
