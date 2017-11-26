0035109007118a2af65b1d18400003b5fdc44743
Bruce Li <405371160@qq.com>
2017-11-26 19:42:02 +0800

    调度例子

--- src/main/java/com/thzj/webcrawler/App.java
+++ src/main/java/com/thzj/webcrawler/App.java
@@ -4,9 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.thzj.webcrawler.repository")
+@EnableScheduling
 @MapperScan("com.thzj.webcrawler.dao")
 public class App {
 
