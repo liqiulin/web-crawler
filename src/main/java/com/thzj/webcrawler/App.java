547d5bb20010b3d0637c0edec45e0941845af446
Bruce Li <405371160@qq.com>
2017-11-26 20:11:46 +0800

    调度任务

--- src/main/java/com/thzj/webcrawler/App.java
+++ src/main/java/com/thzj/webcrawler/App.java
@@ -7,10 +7,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @MapperScan("com.thzj.webcrawler.dao")
 public class App {
-
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
---
+++ src/main/java/com/thzj/webcrawler/job/Scheduler.java
@@ -0,0 +1,51 @@
+package com.thzj.webcrawler.job;
+
+import com.thzj.webcrawler.service.SyncService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+@Component
+@Slf4j
+@EnableScheduling
+public class Scheduler {
+    private static final String SYNC_SWITCH_ON = "1";
+
+    @Value("${sync.switch}")
+    private String syncSwitch;
+    @Resource
+    private SyncService syncService;
+
+    /**
+     * 每晚1点开始同步
+     */
+    @Scheduled(cron="${sync.scheduler.cron}")
+    public void sync() {
+        log.info("scheduled to sync start. swith[{}]", syncSwitch);
+        if (SYNC_SWITCH_ON.equals(syncSwitch)) {
+            syncService.doSync();
+        }
+        log.info("scheduled to sync end. swith[{}]", syncSwitch);
+    }
+
+    /**
+     * 每分钟执行一次
+     */
+    @Scheduled(cron="0 0/1 * * * ?")
+    public void statusCheck() {
+        log.info("每分钟执行一次。开始……");
+        log.info("每分钟执行一次。结束。");
+    }
+
+    @Scheduled(fixedRate=20000)
+    public void testTasks() {
+        log.info("每20秒执行一次。开始……");
+        //statusTask.healthCheck();
+        log.info("每20秒执行一次。结束。");
+    }
+}
