dc7465195e1773a6250bdb19ee3379c75f0af3de
liqiulin <liqiulin@fxiaoke.com>
2017-12-05 19:33:45 +0800

    抓取对象ID测试

--- src/main/java/com/thzj/webcrawler/util/BaseUtil.java
+++ src/main/java/com/thzj/webcrawler/util/BaseUtil.java
@@ -41,7 +41,7 @@ public class BaseUtil {
             if (e.getStatusCode() == HttpStatus.NOT_FOUND.value()) {
                 throw new GrabResourceNotFoundException("404 error. ", e);
             } else {
-                int sleepTime = 30000;
+                int sleepTime = 20000;
                 log.info("retry after[{}]ms", sleepTime);
                 sleep(sleepTime);
                 return connect(url);
