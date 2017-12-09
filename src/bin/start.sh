4663f9b0a99261e78af77a2dc236a3b4f52d88e6
Bruce Li <405371160@qq.com>
2017-12-09 21:18:04 +0800

    设置堆大小

--- src/bin/start.sh
+++ src/bin/start.sh
@@ -12,7 +12,7 @@ export CLASSPATH="$CODE_HOME/classes:$CODE_HOME/lib/*"
 # java可执行文件位置
 export _EXECJAVA="/usr/getd/jdk/jdk1.8.0_151/bin/java"
 # JVM启动参数
-export JAVA_OPTS="-server -Xms128m -Xmx256m -Xss256k -XX:MaxDirectMemorySize=128m"
+export JAVA_OPTS="-server -Xms512m -Xmx512m"
 # 启动类
 export MAIN_CLASS=com.thzj.webcrawler.App
 
